f1edc7a2f3f96645de18f7a41de35bfeefe65ac8
Aditya <1999shakuntala@gmail.com>
2024-06-04 01:22:35 +0530

    Navigation Page and Fragments for Different Navigation Fields

---
+++ app/src/main/java/com/example/madassignment/Bottom_Navigation.java
@@ -0,0 +1,14 @@
+package com.example.madassignment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Bottom_Navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+    }
+}
